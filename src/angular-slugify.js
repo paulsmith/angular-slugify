39e5c453db2bafe80687b868e6d80dc76c2c66d7
William Boman <william@redwill.se>
2015-01-06 20:42:00 +0100

    Move source file to src/. Remove redundant test/ files.

--- src/angular-slugify.js
+++ src/angular-slugify.js
@@ -1,29 +1,27 @@
-/**
- * angular-slugify -- provides slugification for AngularJS
- *
- * Copyright © 2013 Paul Smith <paulsmith@pobox.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the “Software”), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-"use strict";
+/*
+Copyright © 2013 Paul Smith <paulsmith@pobox.com>
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 (function() {
+    "use strict";
+
     var mod = angular.module("slugifier", []);
 
     // Unicode (non-control) characters in the Latin-1 Supplement and Latin
@@ -41,7 +39,9 @@
         'ª': "a",
         '«': "<<",
         '¬': "not",
+        /* jshint -W100 */
         '­': "-",
+        /* jshint +W100 */
         '®': "(R)",
         '°': "^0",
         '±': "+/-",
@@ -265,13 +265,13 @@
         return s.replace(/[-\s]+/g, "-");
     }
 
-    mod.factory("Slug", function() {
+    mod.factory("Slug", function () {
         return {
             slugify: _slugify
         };
     });
 
-    mod.directive("slug", ["Slug", function(Slug) {
+    mod.directive("slug", ["Slug", function (Slug) {
         return {
             restrict: "E",
             scope: {
@@ -291,9 +291,9 @@
         };
     }]);
 
-    mod.filter("slugify", function(Slug) {
-        return function(input) {
+    mod.filter("slugify", ["Slug", function (Slug) {
+        return function (input) {
             return Slug.slugify(input);
         };
-    });
+    }]);
 })();
